704b5f7413d0a41f57303d75b3309929a96aa9f7
danimtnz <danimtnz2004@gmail.com>
2023-10-30 12:28:01 +0100

    retoques en el intento de recyclerview para ver en clase

--- app/src/main/java/com/example/contador/RecyclerviewActivity.java
+++ app/src/main/java/com/example/contador/RecyclerviewActivity.java
@@ -1,8 +1,13 @@
 package com.example.contador;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class RecyclerviewActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class RecyclerviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recyclerview);
+        RecyclerView rv = (RecyclerView) findViewById(R.id.recyclerview);
+        rv.setHasFixedSize(true);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        List<String> lista = Arrays.asList("Gustavo", "Daniel", "Cecilia", "Diego");
+        rv.setAdapter((RecyclerView.Adapter) lista);
     }
 }
